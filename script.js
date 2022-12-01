f62990738d4413516ac9d95511ab1d695a7d9c0c
ccadran <clariocadran33@gmail.com>
2022-12-01 10:31:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const btnBurger = document.querySelector("#btn");
+const body = document.querySelector("body");
+let lastScroll = 0;
+btnBurger.addEventListener("click", () => {
+    btnBurger.classList.toggle("active");
+    nav.classList.toggle("active");
+    logo.classList.toggle("active");
+    if (btnBurger.classList.contains("active")) {
+        console.log(window.scrollY);
+        nav.style.height = window.innerHeight + "px";
+        nav.style.width = window.innerWidth + "px";
+        nav.style.top = window.scrollY + "px";
+        body.style = "overflow:hidden";
+    } else {
+        body.style = "overflow:scroll";
+    }
+});
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY < lastScroll) {
+        btnBurger.style.top = "30px";
+    } else {
+        btnBurger.style.top = "-60px";
+    }
+
+    lastScroll = window.scrollY;
+});
