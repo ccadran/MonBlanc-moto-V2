bb057559c852a110ae2889f7cbd10a6260b5f631
ccadran <clariocadran33@gmail.com>
2022-12-13 12:45:52 +0100

    final

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btnBurger = document.querySelector("#btn");
 const body = document.querySelector("body");
+const header = document.querySelector("header");
 let lastScroll = 0;
 btnBurger.addEventListener("click", () => {
     btnBurger.classList.toggle("active");
@@ -28,7 +29,32 @@ function btnBurgerOnScroll() {
         }
     });
 }
-btnBurgerOnScroll();
+if (innerWidth < 1100) {
+    btnBurgerOnScroll();
+}
+
+like.addEventListener("click", () => {
+    if (like.style.backgroundColor === "white") {
+        like.style.backgroundColor = "#f3ec78";
+        like.style.backgroundImage =
+            "linear-gradient(54deg,rgba(164, 84, 16, 1) 48%,rgba(251, 239, 183, 1) 100%)";
+    } else {
+        like.style.backgroundColor = "white";
+        like.style.backgroundImage = "none";
+    }
+});
+if (innerWidth > 1100) {
+    window.addEventListener("scroll", () => {
+        if (window.scrollY < lastScroll) {
+            header.style.top = 0;
+        } else {
+            header.style.top = "-100px";
+            console.log("test");
+        }
+
+        lastScroll = window.scrollY;
+    });
+}
 // window.addEventListener("scroll", () => {
 //     console.log(window.scrollY);
 //     if (window.scrollY < 60) {
